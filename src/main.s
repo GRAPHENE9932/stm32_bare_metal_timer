4705a01a79fe045ea6d9854fa0a1363dfff40934
Artem Kliminskyi <graphene64@gmail.com>
2024-03-24 13:49:39 +0100

    Write code for the 74hc595 shift register chip.

--- src/main.s
+++ src/main.s
@@ -9,6 +9,10 @@
 .equ GPIOA_MODER_OFFSET, 0x00000000
 .equ GPIOX_ODR_OFFSET, 0x00000014
 
+.equ SER_PORT_NUMBER, 10
+.equ SRCLK_PORT_NUMBER, 9
+.equ RCLK_PORT_NUMBER, 12
+
 @ Accepts no arguments. Returns nothing.
 .type enable_io_porta_clock, %function
 enable_io_porta_clock:
@@ -44,34 +48,26 @@ set_gpioa_moder:
 
 @ Arguments:
 @ R0 - port number.
-@ Returns nothing.
-.type write_high_gpio_odr, %function
-write_high_gpio_odr:
-    ldr R1, =GPIOA_BASE + GPIOX_ODR_OFFSET  @ R1 stores the word location.
-    ldr R2, =1                              @ R2 contains the bit.
-    lsl R2, R2, R0
-    ldr R3, [R1]                            @ R3 stores the word.
-    orr R3, R3, R2
-    str R3, [R1]
+@ R1 - what to write (either 0 or 1).
+@ Returns nothing. Modifies the R0-R4 registers.
+@ Takes 20 cycles in total.
+.type write_to_gpioa_odr, %function
+write_to_gpioa_odr:                         @ 4 cycles to call.
+    ldr R2, =GPIOA_BASE + GPIOX_ODR_OFFSET  @ 2 cycles; R2 stores the ODR location.
+    lsl R1, R1, R0                          @ 1 cycle; R1 stores the OR mask now.
+    ldr R3, =1                              @ 2 cycles; R3 stores the AND mask.
+    lsl R3, R3, R0                          @ 1 cycle.
+    mvn R3, R3                              @ 1 cycle.
+    
+    ldr R4, [R2]                            @ 2 cycles; R4 contains the ODR.
+    and R4, R4, R3                          @ 1 cycle.
+    orr R4, R4, R1                          @ 1 cycle.
+    str R4, [R2]                            @ 2 cycles.
 
-    bx LR
-
-@ Arguments:
-@ R0 - port number.
-@ Returns nothing.
-.type write_low_gpio_odr, %function
-write_low_gpio_odr:
-    ldr R1, =GPIOA_BASE + GPIOX_ODR_OFFSET  @ R1 stores the word location.
-    ldr R2, =0xFFFFFFFE                     @ R2 contains the bit.
-    lsl R2, R2, R0
-    ldr R3, [R1]                            @ R3 stores the word.
-    and R3, R3, R2
-    str R3, [R1]
-
-    bx LR
+    bx LR                                   @ 3 cycles.
 
 @ Accepts no arguments. Returns nothing.
-@ Just wastes 4000001 cycles.
+@ Just wastes 4000000 cycles.
 .type delay_for_4m_cycles, %function
 delay_for_4m_cycles:                @ 4 cycles to call.
     ldr R0, =0                      @ 2 cycles.
@@ -81,25 +77,94 @@ delay_for_4m_cycles_loop:
     cmp R0, R1                      @ 1 cycle.
     blo delay_for_4m_cycles_loop    @ 3 cycle if taken, 1 if not.
 
+    nop                             @ 1 cycle.
     bx LR                           @ 3 cycles.
+
+@ Arguments:
+@ R0 - register that contains the bits.
+@ R1 - how many bits to shift out (8, 16, 24 and 32 are allowed).
+.type shift_out_bits, %function
+shift_out_bits:
+    push {LR}
+
+    mov R5, R0                  @ Move our arguments to R5 and R6 respectively, as write_to_gpioa_odr will modify the R0-R4 registers.
+    mov R6, R1
+
+    ldr R0, =RCLK_PORT_NUMBER   @ Port number.
+    ldr R1, =0                  @ What to write.
+    bl write_to_gpioa_odr
+
+    ldr R2, =0
+    mov R8, R2                  @ R8 is the loop counter.
+shift_out_bits_loop:
+
+    ldr R0, =SER_PORT_NUMBER    @ Port number.
+    ldr R1, =0                  @ What to write.
+    bl write_to_gpioa_odr
+
+    ldr R0, =SRCLK_PORT_NUMBER  @ Port number.
+    ldr R1, =0                  @ What to write.
+    bl write_to_gpioa_odr
+
+    ldr R0, =SER_PORT_NUMBER    @ Port number.
+    mov R1, R5                  @ What to write.
+    ldr R2, =1
+    and R1, R1, R2
+    bl write_to_gpioa_odr
+
+    ldr R0, =SRCLK_PORT_NUMBER  @ Port number.
+    ldr R1, =1                  @ What to write.
+    bl write_to_gpioa_odr
+
+    lsr R5, R5, #1
+    mov R1, R8
+    add R1, R1, #1
+    mov R8, R1
+
+    cmp R8, R6
+    blo shift_out_bits_loop
+@ End of the shift_out_bits_loop
+
+    ldr R0, =SRCLK_PORT_NUMBER  @ Port number.
+    ldr R1, =1                  @ What to write.
+    bl write_to_gpioa_odr
+
+    ldr R0, =RCLK_PORT_NUMBER   @ Port number.
+    ldr R1, =1                  @ What to write.
+    bl write_to_gpioa_odr
+
+    ldr R0, =SER_PORT_NUMBER    @ Port number.
+    ldr R1, =0                  @ What to write.
+    bl write_to_gpioa_odr
+
+    ldr R0, =SRCLK_PORT_NUMBER  @ Port number.
+    ldr R1, =0                  @ What to write.
+    bl write_to_gpioa_odr
+
+    ldr R0, =RCLK_PORT_NUMBER   @ Port number.
+    ldr R1, =0                  @ What to write.
+    bl write_to_gpioa_odr
+
+    pop {PC}
 
 .type main, %function
 main:
     bl enable_io_porta_clock
 
-    ldr R0, =12         @ Port number.
-    ldr R1, =0b01       @ Output mode.
+    ldr R0, =SER_PORT_NUMBER    @ Port number.
+    ldr R1, =0b01               @ Output mode.
     bl set_gpioa_moder
 
-loop:
-    ldr R0, =12         @ Port number.
-    bl write_high_gpio_odr
+    ldr R0, =SRCLK_PORT_NUMBER  @ Port number.
+    ldr R1, =0b01               @ Output mode.
+    bl set_gpioa_moder
 
-    bl delay_for_4m_cycles
+    ldr R0, =RCLK_PORT_NUMBER   @ Port number.
+    ldr R1, =0b01               @ Output mode.
+    bl set_gpioa_moder
 
-    ldr R0, =12         @ Port number.
-    bl write_low_gpio_odr
+    ldr R0, =0b00000000000000001101111111011010  @ Bits.
+    ldr R1, =16             @ How many bits to shift out.
+    bl shift_out_bits
 
-    bl delay_for_4m_cycles
-
-    b loop
+    b main
