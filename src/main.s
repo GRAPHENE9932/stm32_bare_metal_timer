582c21d093ce23c10a4c6114d2ff8087aea1aee3
Artem Kliminskyi <graphene64@gmail.com>
2024-03-31 17:32:49 +0200

    Decrement seconds and minutes every second.

--- src/main.s
+++ src/main.s
@@ -8,14 +8,52 @@ digits:
 .global main
 .global tim3_tick
 
+.type decrement_second, %function
+decrement_second:
+    ldr R0, =digits             @ R0 stores the digits location.
+    ldr R1, [R0]                @ R1 stores the digits.
+    ldr R2, =0x000000FF         @ R2 stores the AND mask to extract digits that represent seconds (D1:D0).
+    
+    mov R3, R1                  @ R3 stores only digits that represent seconds.
+    and R3, R3, R2
+    cmp R3, #0
+    beq decrement_second_minute_over
+    
+    sub R1, R1, #1
+    mov R3, R1
+    ldr R2, =0x0000000F         @ R2 stores the AND mask to extract the D0 digit only.
+    and R3, R3, R2              @ R3 stores the D0 digit now.
+    cmp R3, #0x09
+    ble decrement_second_skip_subtracting_6_from_secs
+    sub R1, #6                  @ F - 9 = 6.
+decrement_second_skip_subtracting_6_from_secs:
+    str R1, [R0]
+    bx LR
+decrement_second_minute_over:
+    mov R3, R1
+    lsr R3, R3, #8              @ R3 stores the minute digits only now.
+    sub R3, R3, #1
+
+    ldr R2, =0x0000000F         @ R2 stores the AND mask to extract the first digit only.
+    and R3, R3, R2
+    cmp R3, #0x09
+    ble decrement_second_skip_subtracting_6_from_mins
+    sub R1, #6
+decrement_second_skip_subtracting_6_from_mins:
+    mov R1, R3
+    lsl R1, R1, #8
+    ldr R2, =0x00000059         @ R2 stores the D1:D0 digits to set.
+    orr R1, R1, R2
+    str R1, [R0]
+    bx LR
+
 .type tim3_tick, %function
 tim3_tick:
-    ldr R0, =digits
-    ldr R1, [R0]
-    add R1, R1, #1
-    str R1, [R0]
+    push {LR}
 
-    bx LR
+    bl decrement_second
+
+    pop {PC}
 
 .type main, %function
 main:
